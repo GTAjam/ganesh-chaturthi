095f7ad197905eec6a69476eace6cbaaa4916716
GTAjam <gtaclub2020@gmail.com>
2020-08-24 16:23:16 +0530

    more

---
+++ p1/Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreup;
+    public EndTrigger ee;
+    // Update is called once per frame
+    void Update()
+    {
+        scoreup.text = "MODAKS : " + Convert.ToString(ee.coin_count);
+    }
+}
